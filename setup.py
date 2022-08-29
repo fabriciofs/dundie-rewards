9181138191e253ee1acab8330b549af40e916f4c
Fabricio Ferreira <fabricio@fabriciofs.eti.br>
2022-08-29 15:00:09 -0300

    add makefile

--- setup.py
+++ setup.py
@@ -1,10 +1,28 @@
+import os
 from setuptools import setup, find_packages
+
+
+def read(*paths):
+    rootpath = os.path.dirname(__file__)
+    filepath = os.path.join(rootpath, *paths)
+    with open(filepath) as file_:
+        return file_.read().strip()
+
+
+def read_requirements(path):
+    return [
+        line.strip()
+        for line in read(path).split("\n")
+        if not line.startswith(("#", "git+", '"', '-'))
+    ]
 
 
 setup(
     name="dundie",
     version="0.1.0",
     description="Reward Point System from Dunder Mifflin",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
     author="Fabricio Ferreira",
     packages=find_packages(),
     entry_points={
@@ -12,4 +30,9 @@ setup(
             "dundie = dundie.__main__:main"
         ]
     },
+    install_requires=read_requirements("requirements.txt"),
+    extras_require={
+        "test": read_requirements("requirements.test.txt"),
+        "dev": read_requirements("requirements.dev.txt")
+    }
 )
