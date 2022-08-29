ff4f0ca271f0c7fab182bc51e8b056bb9f7008d8
Fabricio Ferreira Santos <fabricio@fabriciofs.eti.br>
2022-08-29 09:14:33 -0300

    turned to installable binary #9 (#12)
    closes #9

--- setup.py
+++ setup.py
@@ -7,4 +7,9 @@ setup(
     description="Reward Point System from Dunder Mifflin",
     author="Fabricio Ferreira",
     packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "dundie = dundie.__main__:main"
+        ]
+    },
 )
